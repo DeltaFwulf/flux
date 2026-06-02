further refactored code to separate mesh and lc functions, fixed flux calculation error in conduction.

src/flux_main.py
@@ -5,13 +5,11 @@ from os.path import join
 import yaml
 import numpy as np
 
-from lc_setup import init_lumped_capacitor
-from mesh_setup import init_mesh
-from util import update_mesh, calc_edge_states, update_properties
-from plotter import animate_temp_2d
+from lumped_capacitor import init_lc
+from mesh import init_mesh, update_mesh, calc_edge_states
+from util import update_properties
+from plotter import animate_temp_2d, plot_total_powers
 
-# pass empty dicts for mesh, env, lc if no items needed. This allows
-# for the code to skip the simulation loop (seems simpler than the alternatives)
 
 
 def run_simulation(config:str):
@@ -40,25 +38,26 @@ def run_simulation(config:str):
     runtime.update({'theta':cfg['theta']})
     runtime.update({'max_courant':cfg['max_courant']})
 
+    config = {'runtime':runtime}
+
     # initialise meshes
     meshes = {}
-    for key, mesh in cfg['meshes'].items():
-        meshes.update({key:init_mesh(mesh, cfg['force_finer'])})
+    if cfg.get('meshes') is not None:
+        for key, mesh in cfg['meshes'].items():
+            meshes.update({key:init_mesh(mesh, cfg['force_finer'])})
+        config.update({'meshes':meshes})
 
     # initialise lumped capacitors
     lumped_capacitors = {}
-    for key, lc in cfg['lumped_capacitors'].items():
-        lumped_capacitors.update({'key':init_lumped_capacitor(lc)})
+    if cfg.get('lumped_capacitors') is not None:
+        for key, lc in cfg['lumped_capacitors'].items():
+            lumped_capacitors.update({'key':init_lc(lc)})
+        config.update({'lumped_capacitors':lumped_capacitors})
 
-    environment = cfg['environment']
+    if cfg.get('environment') is not None:
+        environment = cfg['environment']
+        config.update({'environment':environment})
 
-    # TODO: check that values are updated through simulation
-    config = {}
-    config.update({'runtime':runtime})
-    config.update({'meshes':meshes})
-    config.update({'lumped_capacitors':lumped_capacitors})
-    config.update({'environment':environment})
-    
     # Run Simulation -----------------------------------------------------------------------------#
     #                                                                                             #
     # --------------------------------------------------------------------------------------------#
@@ -82,9 +81,8 @@ def run_simulation(config:str):
         # update meshes
         for m in meshes.values():
             m['u_last'] = m['u_latest']
-            update_properties(m)
 
-            # TODO: calculate powers at each edge / face
+            update_properties(m)
             calc_edge_states(cfg=config)
 
             m['u_latest'] = update_mesh(mesh=m,
@@ -95,63 +93,23 @@ def run_simulation(config:str):
             if store:
                 m['u'] = np.dstack((m['u'], m['u_latest']))
 
+                for e in range(len(m['edges'])):                    
+                    m['edge_fluxes'][e] = np.append(m['edge_fluxes'][e],m['edge_fluxes_latest'][e])
+                    m['edge_powers'][e] = np.append(m['edge_powers'][e],m['edge_powers_latest'][e])
+
         # update lumped capacitors
-        for c in lumped_capacitors.values():
-            c['u_last'] = c['u_latest']
-            update_properties(c)
-            # TODO: calculate powers at each edge
-            # TODO: update temperature per sensible enthalpy
+        # for c in lumped_capacitors.values():
+        #     c['u_last'] = c['u_latest']
+        #     update_properties(c)
+        #     # TODO: calculate powers at each edge
+        #     # TODO: update temperature per sensible enthalpy
 
     results = config
     results.update({'t':t})
 
     # plotting / rendering
     animate_temp_2d(results, save=False)
-
-
-
-def simulate_2d(inputs:dict) -> None:
-    """Simulates multiple interacting 2D objects."""
-
-    t = np.array([0.0], float)
-    t_now = 0.0
-
-    runtime = inputs['runtime']
-    meshes = inputs.get('meshes')
-    # caps = inputs.get('lumped_capacitors')
-
-    while t_now < runtime['tf']:
-        # calculate next timestep to satisfy courant constraint
-        dt = min(runtime['dt_storage'], min(runtime['max_courant']*min(m['dx'],\
-                 m['dy'])**2 / np.max(m['diffusivity']) for m in meshes.values()))
-
-        t_now += dt
-        print("                                     ", end='\r')
-        print(f"t = {t_now:0.3f} s, dt = {dt:0.6f} s", end='\r')
-        store = t_now - t[-1] >= runtime['dt_storage']
-        if store:
-            t = np.hstack((t, t_now))
-
-        for m in meshes.values():
-
-            m['u_last'] = m['u_latest']
-            update_properties(m)
-            calc_edge_states(cfg=inputs)
-
-            m['u_latest'] = update_mesh(mesh=m,
-                                        dt=dt,
-                                        curv=m['curvature'],
-                                        theta=runtime['theta'])
-
-            if store:
-                m['u'] = np.dstack((m['u'], m['u_latest']))
-
-    outputs = {}
-    outputs.update({'meshes':meshes})
-    outputs.update({'t':t})
-
-    return outputs
-
+    plot_total_powers(results)
 
 
 run_simulation('octoforge.yaml')

src/heat_transfer.py
@@ -6,13 +6,16 @@ import numpy as np
 from pyfluids import Fluid, FluidsList, Input, HumidAir, InputHumidAir
 
 
-def conduction(edge:dict, link:dict) -> np.ndarray:
-    """Calculates common interface temperature between conducting faces."""
+def conduction(edge:dict, link:dict) -> dict:
+    """Calculates common interface temperature between conducting faces and flux."""
 
     na = link['k_bar'] / link['hn']
     nb = edge['k_bar'] / edge['hn']
 
-    return (na*link['u_in'] + nb*edge['u_in']) / (na + nb)
+    u_int = (na*link['u_in'] + nb*edge['u_in']) / (na + nb)
+    q = nb*(u_int - edge['u_in'])
+
+    return {'q':q, 'u_int':u_int}
 
 
 